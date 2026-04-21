42611267a5bedd457cf33d55f26b9d54bc9adc10
viperofficialll <sharadhapokharel777@gmail.com>
2026-04-21 13:45:39 +0545

    refactor(users-service): wrap service inside parent folder

--- src/schemas/loginSchema.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod";
-
-export const userLoginSchema = z.object({
-    email: z.string().email().min(1),
-    password: z.string(),
-}); 
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app.js";
-import { seedAdmin } from "../prisma/seed.js";
-
-const PORT = process.env.PORT || 3000;
-
-await seedAdmin();
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
